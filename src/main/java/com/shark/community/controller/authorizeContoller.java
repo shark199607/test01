78037fabba19f679287f1ed4f19002cbc7feae19
shark <475910597@qq.com>
2020-05-28 09:44:59 +0800

    check

--- src/main/java/com/shark/community/controller/authorizeContoller.java
+++ src/main/java/com/shark/community/controller/authorizeContoller.java
@@ -9,11 +9,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
 @Controller
+
 public class authorizeContoller {
+
 
     @Autowired
     private githubProvide githubProvide;
@@ -29,15 +32,19 @@ public class authorizeContoller {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state) throws IOException {
+
         accessTokenDto accessTokenDto = new accessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
         accessTokenDto.setRedirect_url(redirectURL);
+        accessTokenDto.setState(state);
 
         String accessToken = githubProvide.getAccessToken(accessTokenDto);
+
         githubUser user=githubProvide.getUser(accessToken);
         System.out.println(user.getName());
+
         return "index";
     }
 
--- src/main/java/com/shark/community/provider/githubProvide.java
+++ src/main/java/com/shark/community/provider/githubProvide.java
@@ -6,11 +6,12 @@ import com.shark.community.dto.githubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
+import javax.sound.midi.Soundbank;
 import java.io.IOException;
 
 @Component
 public class githubProvide {
-    public String getAccessToken(accessTokenDto accessTokenDto){
+    public String getAccessToken(accessTokenDto accessTokenDto) {
         MediaType mediaType
                 = MediaType.get("application/json; charset=utf-8");
 
@@ -20,27 +21,29 @@ public class githubProvide {
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
-            try (Response response = client.newCall(request).execute()) {
-                String string =response.body().string();
-                String token= string.split("&")[0].split("=")[1];
-                return token;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            System.out.println(token);
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+
     public githubUser getUser(String accessToken) throws IOException {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token="+accessToken)
+                .url("https://api.github.com/user?access_token=" + accessToken)
                 .build();
-        try (Response response = client.newCall(request).execute()) {
-            String string=response.body().string();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
             githubUser githubUser = JSON.parseObject(string, githubUser.class);
             return githubUser;
+        } catch (IOException e) {
         }
-
-
+        return null;
     }
-
 }
